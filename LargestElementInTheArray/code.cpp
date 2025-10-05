d0b1359f640fa3f71ec0f85fa10d1551a71198e1
Aman Parganiha <72440381+amanparganiha@users.noreply.github.com>
2025-10-05 22:17:11 +0530

    Update code.cpp

--- LargestElementInTheArray/code.cpp
+++ LargestElementInTheArray/code.cpp
@@ -2,12 +2,12 @@ class Solution {
   public:
     int largest(vector<int> &arr) {
         int n = arr.size();
-        vector<int> :: iterator largest = arr.begin() ;
-        for(int i = 1 ; i < n ; i++){
-            if( *largest <= arr[i]){
-                *largest = arr[i];
-            }
+        int largest = arr[0];
+        for(int i = 0 ; i < n ; i++){
+          if(largest <= arr[i]){
+            largest = arr[i];
+          }
         }
-        return *largest;
+        return largest;
     }
 };
